c9f9a7feedc5fe1cb24209c9b1b63a5e9b72730a
ferdousanam <ferdous.anam@gmail.com>
2020-02-09 02:01:00 +0600

    Reverted logic in generate_multilevel_menu helper functionality

--- src/app/Http/Helpers/Menus.php
+++ src/app/Http/Helpers/Menus.php
@@ -13,52 +13,47 @@ function generate_multilevel_menus($parent_id = 0)
 
     if ($menues->count() > 0) {
         foreach ($menues as $key => $menu) {
-            /**
-             * Now we check only the visibility of the menus
-             * We do not need to check the priority now
-             */
-            // $has_access = DB::table('priority_menu')->where('priority_id', $priority_id)->where('menu_id', $menu->id)->first();
-            // if ($has_access) {
-            if ($menu->route_name && $menu->route_name == '#') {
-                $html .= '<li id="' . $menu->selector . '-mm" class="kt-menu__item" aria-haspopup="true" data-ktmenu-submenu-toggle="hover">
-  <a href="javascript:;" class="kt-menu__link kt-menu__toggle">';
-                if ($menu->parent_id == 0) {
-                    if ($menu->icon) {
-                        $html .= '<i class="kt-menu__link-icon ' . $menu->icon . '"></i>';
+            $has_access = DB::table('priority_menu')->where('priority_id', $priority_id)->where('menu_id', $menu->id)->first();
+            if ($has_access) {
+                if ($menu->route_name && $menu->route_name == '#') {
+                    $html .= '<li id="' . $menu->selector . '-mm" class="kt-menu__item" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="kt-menu__link kt-menu__toggle">';
+                    if ($menu->parent_id == 0) {
+                        if ($menu->icon) {
+                            $html .= '<i class="kt-menu__link-icon ' . $menu->icon . '"></i>';
+                        }
+                    } else {
+                        $html .= '<i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i>';
                     }
+                    $html .= '<span class="kt-menu__link-text">';
+                    $html .= $menu->menu_name; // $menu->parent_id
+                    $html .= '</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>';
+                    $html .= '<div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>';
+                    $html .= '<ul class="kt-menu__subnav">';
+                    $html .= '<li class="kt-menu__item  kt-menu__item--parent" aria-haspopup="true">';
+                    $html .= '<span class="kt-menu__link"><span class="kt-menu__link-text">User</span></span>';
+                    $html .= '</li>';
+                    $html .= generate_multilevel_menus($menu->id);
+                    $html .= '</ul>';
+                    $html .= '</div>';
+                    $html .= '</li>';
                 } else {
-                    $html .= '<i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i>';
-                }
-                $html .= '<span class="kt-menu__link-text">';
-                $html .= $menu->menu_name; // $menu->parent_id
-                $html .= '</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>';
-                $html .= '<div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>';
-                $html .= '<ul class="kt-menu__subnav">';
-                $html .= '<li class="kt-menu__item  kt-menu__item--parent" aria-haspopup="true">';
-                $html .= '<span class="kt-menu__link"><span class="kt-menu__link-text">User</span></span>';
-                $html .= '</li>';
-                $html .= generate_multilevel_menus($menu->id);
-                $html .= '</ul>';
-                $html .= '</div>';
-                $html .= '</li>';
-            } else {
-                if ($menu->parent_id == 0) {
-                    $html .= '<li id="' . $menu->selector . '-mm" class="kt-menu__item " aria-haspopup="true">';
-                    $html .= '<a href="' . url($menu->route_name) . '" class="kt-menu__link ">';
-                    if ($menu->icon) {
-                        $html .= '<i class="kt-menu__link-icon ' . $menu->icon . '"></i>';
+                    if ($menu->parent_id == 0) {
+                        $html .= '<li id="' . $menu->selector . '-mm" class="kt-menu__item " aria-haspopup="true">';
+                        $html .= '<a href="' . url($menu->route_name) . '" class="kt-menu__link ">';
+                        if ($menu->icon) {
+                            $html .= '<i class="kt-menu__link-icon ' . $menu->icon . '"></i>';
+                        }
+                    } else {
+                        $html .= '<li id="' . $menu->selector . '-sm" class="kt-menu__item " aria-haspopup="true">';
+                        $html .= '<a href="' . url($menu->route_name) . '" class="kt-menu__link ">';
+                        $html .= '<i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i>';
                     }
-                } else {
-                    $html .= '<li id="' . $menu->selector . '-sm" class="kt-menu__item " aria-haspopup="true">';
-                    $html .= '<a href="' . url($menu->route_name) . '" class="kt-menu__link ">';
-                    $html .= '<i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i>';
+                    $html .= '<span class="kt-menu__link-text">';
+                    $html .= $menu->menu_name; // $menu->parent_id
+                    $html .= '</span></a></li>';
+                    $html .= generate_multilevel_menus($menu->id);
                 }
-                $html .= '<span class="kt-menu__link-text">';
-                $html .= $menu->menu_name; // $menu->parent_id
-                $html .= '</span></a></li>';
-                $html .= generate_multilevel_menus($menu->id);
             }
-            // }
         }
     }
     return $html;
