d8deddf3f5b6805e4a9de5107810b915f27a741e
Ferdous Anam <ferdous.anam@gmail.com>
2020-10-15 00:33:02 +0600

    Custom Menus Option

--- src/views/layouts/partials/_aside/_menu.blade.php
+++ src/views/layouts/partials/_aside/_menu.blade.php
@@ -3,6 +3,7 @@
   <div id="kt_aside_menu" class="kt-aside-menu " data-ktmenu-vertical="1" data-ktmenu-scroll="1" data-ktmenu-dropdown-timeout="500">
     <ul class="kt-menu__nav ">
       {!! indent(generate_multilevel_menus()) !!}
+      @stack('custom-menus')
       @if(super_user())
         @include('dashboard::layouts.inc.superUserMenus')
       @endif
